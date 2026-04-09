ade1bfe5a4d1ff7a7988370ffcc2bae2de1d1444
AlisaPrtkv <alisaevgesha@gmail.com>
2026-04-09 23:17:30 +1000

    Update 3.c

---
+++ lab3/3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str[80];
+    int i;
+
+    printf("Enter the string (up to 80 characters): ");
+    scanf("%80s", &str);
+
+    printf("Entered string: %s \n", str);
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'a')
+        {
+            str[i] = 'A';
+        }
+        if (str[i] == 'b')
+        {
+            str[i] = 'B';
+        }
+    }
+
+    printf("The result: %s \n", str);
+    
+    return 0;
+}
